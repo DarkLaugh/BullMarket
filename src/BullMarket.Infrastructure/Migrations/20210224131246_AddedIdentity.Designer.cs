b27fb639274b0b3d090383448c693c3ea855c38d
Alexander Yonchev <24881674+ayonchev@users.noreply.github.com>
2021-02-24 15:17:15 +0200

    Added identity and downgraded the versions of some packages.

--- src/BullMarket.Infrastructure/Migrations/20210224131246_AddedIdentity.Designer.cs
+++ src/BullMarket.Infrastructure/Migrations/20210224131246_AddedIdentity.Designer.cs
@@ -4,14 +4,16 @@ using BullMarket.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BullMarket.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210224131246_AddedIdentity")]
+    partial class AddedIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
