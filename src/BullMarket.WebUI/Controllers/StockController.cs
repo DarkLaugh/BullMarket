09d8fac110acc47c50bd917f6b08eb9a995b698b
Alexander Yonchev <24881674+ayonchev@users.noreply.github.com>
2021-02-25 17:24:38 +0200

    Added a stock controller.

---
+++ src/BullMarket.WebUI/Controllers/StockController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BullMarket.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BullMarket.WebUI.Controllers
+{
+    [Route("api/stocks")]
+    [ApiController]
+    [Authorize]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+
+        public StockController(IStockService stockService)
+        {
+            this._stockService = stockService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetStocks()
+        {
+            var result = await _stockService.GetAllStocksPaginatedAsync();
+
+            return Ok(result);
+        }
+    }
+}
