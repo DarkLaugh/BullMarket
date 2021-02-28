f69413113e56dc2c079d5fb0b7dae2c45ba2db4c
Gencho Draganov <gencho.draganov.uni@gmail.com>
2021-03-01 00:08:51 +0200

    Added an AddStockComment method to the Hub and an AddedComment method to the Client

--- src/BullMarket.WebUI/Hubs/StockHub.cs
+++ src/BullMarket.WebUI/Hubs/StockHub.cs
@@ -1,13 +1,28 @@
-﻿using BullMarket.WebUI.Hubs.Clients;
+﻿using BullMarket.Application.DTOs.Requests;
+using BullMarket.Application.Interfaces.Clients;
+using BullMarket.Application.Interfaces.Services;
 using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace BullMarket.WebUI.Hubs
 {
     public class StockHub : Hub<IStockClient>
     {
+        private IStockService _stockService { get; }
+
+        public StockHub(IStockService stockService)
+        {
+            _stockService = stockService;
+        }
+
+        public async Task AddStockComment(CommentRequest commentRequest)
+        {
+            var savedComment = await _stockService.AddCommentToStock(commentRequest);
+
+            if (savedComment != null)
+            {
+                await this.Clients.All.AddedComment(savedComment);
+            }
+        }
     }
 }
