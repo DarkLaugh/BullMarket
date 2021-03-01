e9dbd75a3b667a5eb3ac5418cd4044013eaeb1c1
Alexander Yonchev <24881674+ayonchev@users.noreply.github.com>
2021-03-01 12:53:21 +0200

    Added a stock changed response which is streamed by the hub.

---
+++ src/BullMarket.Application/DTOs/Responses/StockChangedResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BullMarket.Application.DTOs.Responses
+{
+    public class StockChangedResponse
+    {
+        public string Symbol { get; set; }
+        public decimal Price { get; set; }
+    }
+}
