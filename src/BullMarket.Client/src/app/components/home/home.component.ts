aa3db4d9eb15c5e7feea3aff8394c94f4b22451b
Alexander Yonchev <24881674+ayonchev@users.noreply.github.com>
2021-03-01 18:21:27 +0200

    Added price changing and navigation to a particular stock.

--- src/BullMarket.Client/src/app/components/home/home.component.ts
+++ src/BullMarket.Client/src/app/components/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { HubEvents } from 'src/app/constants/hub.constants';
 import { StockModel } from 'src/app/models/stock/stock.model';
+import { StockChangedModel } from 'src/app/models/stock/stock-changed.model';
 import { StockRealTimeService } from 'src/app/services/real-time/stock-real-time.service';
 import { StockRestService } from 'src/app/services/rest/stock-rest.service';
 
@@ -9,12 +11,19 @@ import { StockRestService } from 'src/app/services/rest/stock-rest.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   stocks: StockModel[] = [];
 
   constructor(
     private restService: StockRestService,
-    private realTimeService: StockRealTimeService) { }
+    private realTimeService: StockRealTimeService,
+    private router: Router) { }
+
+  ngOnDestroy(): void {
+    this.realTimeService
+      .connection
+      .off(HubEvents.StockUpdate);
+  }
 
   ngOnInit(): void {
     this.restService
@@ -25,8 +34,16 @@ export class HomeComponent implements OnInit {
 
     this.realTimeService
       .connection
-      .on(HubEvents.StockUpdate, data => {
+      .on(HubEvents.StockUpdate, (data: StockChangedModel) => {
+        let stock = this.stocks.find(s => s.symbol === data.symbol);
+
+        if(stock.price !== data.price) {
+          stock.price = data.price;
+        }
       });
   }
 
+  selectStock(stock: StockModel) {
+    this.router.navigate(['stock', stock.id]);
+  }
 }
